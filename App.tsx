89da98af425381480c0e55f535ced160b6ab748d
David Osemwegie <35180584+davidosemwegie@users.noreply.github.com>
2023-07-22 16:56:02 -0600

    added firebase support

--- App.tsx
+++ App.tsx
@@ -1,12 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, Text, View } from 'react-native';
+import { View } from 'react-native';
+import { MainLayout } from '@layouts';
+
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-red-500">
-      <Text>Open up App.js to start working on your app wagwan</Text>
-      <StatusBar style="auto" />
-      <Button title='Click me' onPress={() => console.log("Hello world")} />
+    <View className="flex-1 items-center justify-center ">
+      <MainLayout />
     </View>
   );
 }
---
+++ src/layouts/main-layout.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export const MainLayout = () => {
+    return (
+        <View className='bg-black flex-1 w-full'>
+            <Text>Open up App.js to start working on your app wagwan</Text>
+        </View>
+    )
+}
