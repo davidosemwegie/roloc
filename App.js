6e5fc493c1b8ea9ddbff32c82b98fb10632d6550
David Osemwegie <35180584+davidosemwegie@users.noreply.github.com>
2023-07-22 15:42:04 -0600

    added .env

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app! This should work</Text>
       <StatusBar style="auto" />
+      <Button title='Click me' onPress={() => console.log("Hello world")} />
     </View>
   );
 }
