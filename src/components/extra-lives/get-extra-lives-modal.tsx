651f73338d0304e306867144ce4dd6ebba214270
David Osemwegie <35180584+davidosemwegie@users.noreply.github.com>
2023-07-29 18:18:49 -0600

    Update get-extra-lives-modal.tsx

--- src/components/extra-lives/get-extra-lives-modal.tsx
+++ src/components/extra-lives/get-extra-lives-modal.tsx
@@ -1,20 +1,35 @@
 import React, { useState, useEffect } from "react";
-import { Text, View, Dimensions, Button } from "react-native";
+import { View, Button } from "react-native";
 import Popover from 'react-native-popover-view';
 import { PulsingButton } from "../pulsing-button";
 import { Typography } from "../typography";
 import { useAdContext } from "@layouts";
 import { useExtraLifeStore } from "@stores";
 import { TextInput } from "react-native-gesture-handler";
-import { getUserEmail, setUserEmail } from "@fb";
+import { getUserEmail, setUserEmail, trackEvent } from "@fb";
 import { A } from '@expo/html-elements';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 export const GetExtraLivesModal = () => {
     const [showPopover, setShowPopover] = useState(false);
     const [isLoadingAd, setIsLoadingAd] = useState(false); // New state for ad loading
     const [email, setEmail] = useState(''); // New state for ad loading
-    const [dbEmail, setDbEmail] = useState(''); // New state for ad loading
+    const [dbEmail, setDbEmail] = useState(''); // New state for ad loa0ding
+
+    const [lastInstagramClickTime, setLastInstagramClickTime] = useState(0);
+
+    useEffect(() => {
+        async function loadLastInstagramClickTime() {
+            // Load the last clicked time from local storage when the component mounts
+            const lastClickTime = await AsyncStorage.getItem('instagram_link_clicked');
+            if (lastClickTime) {
+                setLastInstagramClickTime(parseInt(lastClickTime, 10));
+            }
+        }
+
+        loadLastInstagramClickTime();
+    }, []);
 
 
     const { extraLives, setExtraLives, addExtraLife } = useExtraLifeStore();
@@ -66,6 +81,70 @@ export const GetExtraLivesModal = () => {
         }
     };
 
+    const handleInstagramLinkClick = async () => {
+        trackEvent('social_link_clicked', {
+            social: 'instagram'
+        });
+        // Check if the current time is greater than the last clicked time plus 12 hours
+        const currentTime = Date.now();
+        if (currentTime - lastInstagramClickTime >= 12 * 60 * 60 * 1000) {
+            // User is allowed to click the link
+            setLastInstagramClickTime(currentTime);
+
+            // Store the new last clicked time in local storage
+            await AsyncStorage.setItem('instagram_link_clicked', currentTime.toString());
+
+            // Perform other actions for the Instagram link click here (e.g., addExtraLife())
+            addExtraLife();
+
+            alert("You got an extra life!");
+
+        } else {
+            // User cannot click the link yet, display a message or take other actions
+            alert("Get extra lives by clicking the link once every 12 hours");
+        }
+    };
+
+    const [lastTwitterClickTime, setLastTwitterClickTime] = useState(0);
+
+    useEffect(() => {
+        async function loadLastTwitterClickTime() {
+            // Load the last clicked time from local storage when the component mounts
+            const lastClickTime = await AsyncStorage.getItem('twitter_link_clicked');
+            if (lastClickTime) {
+                setLastTwitterClickTime(parseInt(lastClickTime, 10));
+            }
+        }
+
+        loadLastTwitterClickTime();
+    }, []);
+
+    const handleTwitterLinkClick = async () => {
+        trackEvent('social_link_clicked', {
+            social: 'twitter'
+        });        // Check if the current time is greater than the last clicked time plus 12 hours
+        const currentTime = Date.now();
+        if (currentTime - lastTwitterClickTime >= 12 * 60 * 60 * 1000) {
+            // User is allowed to click the link
+            setLastTwitterClickTime(currentTime);
+
+            // Store the new last clicked time in local storage
+            await AsyncStorage.setItem('twitter_link_clicked', currentTime.toString());
+
+            // Perform other actions for the Twitter link click here (e.g., addExtraLife())
+            addExtraLife();
+
+            alert("You got an extra life!");
+        } else {
+            // User cannot click the link yet, display a message or take other actions
+            alert("Get extra lives by clicking the Twitter link once every 12 hours");
+        }
+    };
+
+    const isInstagramLinkClickable = Date.now() - lastInstagramClickTime >= 12 * 60 * 60 * 1000;
+    const isTwitterLinkClickable = Date.now() - lastTwitterClickTime >= 12 * 60 * 60 * 1000;
+
+
     return (
         <>
             <PulsingButton
@@ -101,10 +180,10 @@ export const GetExtraLivesModal = () => {
                         Loading...
                     </Typography>
                 ) : rewardedInterstitialAd.isLoaded ? (
-                    <View>
+                    <View className="mt-6">
                         <Button
                             onPress={onWatchAdButtonClicked}
-                            title="Watch an ad"
+                            title="Watch ad to get an extra life"
                         />
                     </View>
                 ) : (
@@ -138,33 +217,50 @@ export const GetExtraLivesModal = () => {
                                     disabled={!email}
                                     onPress={() => {
                                         setUserEmail(email).then(() => {
-                                            alert('Email saved!');
+                                            alert('Email saved! and you got an extra life!');
                                             setDbEmail(email);
                                             setEmail('');
                                         })
+                                        trackEvent('email_saved')
                                     }}
                                 />
                             </>
                         )
                     )}
                 </View>
-                <View className="space-y-6 flex items-center">
-                    <Typography className="text-[18px] text-center">
-                        Hi I'm David, the creator of this game 👋🏽
-                    </Typography>
-                    <A href="https://instagram.com/osazi"
-                        style={{
-                            color: '#1d4ed8',
-                            fontSize: 18,
-                        }}
-                    >Follow me on instagram</A>
-                    <A href="https://twitter.com/@davidosemwegie"
-                        style={{
-                            color: '#1d4ed8',
-                            fontSize: 18,
-                        }}
-                    >Follow me on Twitter</A>
-                </View>
+
+                {(isInstagramLinkClickable || isTwitterLinkClickable) && (
+                    <View className="space-y-6 flex items-center">
+                        <Typography className="text-[18px] text-center">
+                            Hi I'm David, the creator of this game 👋🏽
+                        </Typography>
+                        {isInstagramLinkClickable && (
+                            <A
+                                href="https://instagram.com/osazi"
+                                style={{
+                                    color: '#1d4ed8',
+                                    fontSize: 18,
+                                }}
+                                onPress={handleInstagramLinkClick} // Use the custom handler for the Instagram link
+                            >Follow me on instagram</A>
+                        )}
+
+                        {isTwitterLinkClickable && (
+                            <A href="https://twitter.com/@davidosemwegie"
+                                style={{
+                                    color: '#1d4ed8',
+                                    fontSize: 18,
+                                }}
+                                onPress={handleTwitterLinkClick} // Use the custom handler for the Twitter link
+
+                            >
+                                Follow me on Twitter
+                            </A>
+                        )}
+
+                    </View>
+                )}
+
                 <View className="mt-10">
                     <PulsingButton
                         onPress={() => setShowPopover(false)}
